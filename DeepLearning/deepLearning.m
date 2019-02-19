a82208613490c572260a8f1ee3cf48b06895aa0b
yaelya <yael.yazdi96@gmail.com>
2019-02-19 14:14:15 +0200

    deep lerning

--- DeepLearning/deepLearning.m
+++ DeepLearning/deepLearning.m
@@ -6,18 +6,18 @@ Y = data(:,end);
 X = data(:,1:end-1);
 
 %data(:,1:end-1) = [];
-tabulate(Y)
+%tabulate(Y);
 
-rng(10,'twister')         % For reproducibility
+rng(10,'twister');         % For reproducibility
 part = cvpartition(Y,'Holdout',0.20);
 istrain = training(part); % Data for fitting
 istest = test(part);      % Data for quality assessment
-tabulate(Y(istrain))
+%tabulate(Y(istrain));
 
 N = sum(istrain);         % Number of observations in the training sample
 t = templateTree('MaxNumSplits',N);
 rusTree = fitcensemble(data(istrain,1:18),Y(istrain),'Method','RUSBoost', ...
-    'NumLearningCycles',1000,'Learners',t,'LearnRate',0.1,'nprint',100);
+    'NumLearningCycles',300,'Learners',t,'LearnRate',0.1,'nprint',100);
 figure;
 plot(loss(rusTree,data(istest,1:18),Y(istest),'mode','cumulative'));
 grid on;
@@ -26,21 +26,21 @@ ylabel('Test classification error');
 
 Yfit = predict(rusTree,data(istrain,1:18));
 tab = tabulate(Y(istrain));
-mat = bsxfun(@rdivide,confusionmat(Y(istrain),Yfit),tab(:,2))*100
+mat = bsxfun(@rdivide,confusionmat(Y(istrain),Yfit),tab(:,2))*100;
 
 
 cmpctRus = compact(rusTree);
 
 sz(1) = whos('rusTree');
 sz(2) = whos('cmpctRus');
-[sz(1).bytes sz(2).bytes]
+[sz(1).bytes sz(2).bytes];
 cmpctRus = compact(rusTree);
 
 sz(1) = whos('rusTree');
 sz(2) = whos('cmpctRus');
-[sz(1).bytes sz(2).bytes]
+[sz(1).bytes sz(2).bytes];
 
-L=loss(cmpctRus,data(istrain,1:18),Y(istrain))
+L=loss(cmpctRus,data(istrain,1:18),Y(istrain));
 
 %X = sprintf('accuracy: %f\n', mean(diag(cmpctRus)));
 %disp(X)?
